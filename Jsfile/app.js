455e143cbb5e30aff0df07c8b79e72c3a6c37fb4
Mak-43 <letsmailmak@gmail.com>
2022-02-17 16:09:41 +0600

    js calculation

--- Jsfile/app.js
+++ Jsfile/app.js
@@ -1,35 +1,84 @@
+const minus = document.getElementById('negative-value')
+const exccedIncome = document.getElementById('expenses-over')
+const exceedBalance=document.getElementById('exceed-balance')
+exccedIncome.style.display='none'
+minus.style.display = 'none'
+exceedBalance.style.display = 'none'
 
-document.getElementById('calculate').addEventListener('click',function(){
+
+function error() {
+    const err = document.getElementsByClassName('errors')
+
+
+    for (var i = 0; i < err.length; i++) {
+        if (err[i].value < 0) {
+            
+            return false
+        }
+        
+
+    }
+}
+function percetError(){
+
     
-    const income=parseInt(document.getElementById('income').value)
-    const food=parseInt(document.getElementById('food').value)
-    const rent=parseInt(document.getElementById('rent').value)
-    const cloths=parseInt(document.getElementById('cloth').value)
-    const totalExpenses=document.getElementById('total-expenses') 
-    const balanceAfterpurchase=document.getElementById('balance')
-    const total=food+rent+cloths
-    const balanceAfterpurchaseField=income-total
 
-    balanceAfterpurchase.innerText=balanceAfterpurchaseField
-    totalExpenses.innerText=total
+}
+document.getElementById('calculate').addEventListener('click', function () {
 
-    console.log(total)
+    error()
+
+    if (error() == false) {
+      
+        exccedIncome.style.display='none'
+        minus.style.display = 'block'
+
+
+    }
+
+    if (error() != false) {
+        exccedIncome.style.display='none'
+        minus.style.display = 'none'
+        const income = parseInt(document.getElementById('income').value)
+        const food = parseInt(document.getElementById('food').value)
+        const rent = parseInt(document.getElementById('rent').value)
+        const cloths = parseInt(document.getElementById('cloth').value)
+        const totalExpenses = document.getElementById('total-expenses')
+        const balanceAfterpurchase = document.getElementById('balance')
+        const total = food + rent + cloths
+        const balanceAfterpurchaseField = income - total
+
+        balanceAfterpurchase.innerText = balanceAfterpurchaseField
+        totalExpenses.innerText = total
+        if(income<total)
+        {
+            exccedIncome.style.display='block'
+        }
+    }
+
+
+
 })
 
 
-document.getElementById('saved-buttond').addEventListener('click',function(){
-    const income=parseInt(document.getElementById('income').value)
-    const percentInput=parseInt(document.getElementById('save-input').value)
-    const savingAmount=document.getElementById('saving-amount')
-    const remainBalance=document.getElementById('remaining')
-    const balanceUpdate=parseInt(document.getElementById('balance').innerText)
+document.getElementById('saved-buttond').addEventListener('click', function () {
+   
+    const income = parseInt(document.getElementById('income').value)
+    const percentInput = parseInt(document.getElementById('save-input').value)
+    const savingAmount = document.getElementById('saving-amount')
+    const remainBalance = document.getElementById('remaining')
+    const balanceUpdate = parseInt(document.getElementById('balance').innerText)
+
+    const percent = (percentInput / 100) * income
+    savingAmount.innerText = percent
+    remainBalance.innerText = balanceUpdate - parseInt(savingAmount.innerText)
+    
+    if(balanceUpdate<savingAmount){
+        exceedBalance.style.display='block'
+    }
     
 
-    const percent=(percentInput/100)*income
-    savingAmount.innerText=percent
-    remainBalance.innerText=balanceUpdate-parseInt(savingAmount.innerText)
-    
-    
 
-    console.log(remainBalance.innerText)
+
+
 })
